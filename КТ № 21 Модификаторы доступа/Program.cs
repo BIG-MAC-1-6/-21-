49f1a6c0c5bf0ca6c8e635eaa25e19eeece61ce8
Nikei <Nikei@DESKTOP-NH3O0C1>
2024-12-18 12:32:08 +1000

    Добавьте файлы проекта.

---
+++ КТ № 21 Модификаторы доступа/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace КТ___21_Модификаторы_доступа
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Student student1 = new Student("Иван Иванов", 20, "ИВТ-1");
+            student1.PrintInfo();
+
+            student1.Name = "Петр Петров";
+            student1.Age = 22;
+
+            student1.PrintInfo();
+
+            Console.ReadKey();
+
+        }
+    }
+
+}
---
+++ КТ № 21 Модификаторы доступа/Student.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace КТ___21_Модификаторы_доступа
+{
+    public class Student
+    {
+        public string Name;
+        private int age;
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value >= 0)
+                {
+                    age = value;
+                }
+                else
+                {
+                    Console.WriteLine("Возраст не может быть отрицательным!");
+                }
+            }
+        }
+
+        protected string Group;
+        public Student(string name, int age, string group)
+        {
+            Name = name;
+            Age = age;
+            Group = group;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Имя: {Name}, Возраст: {Age}, Группа: {Group}");
+        }
+
+        protected void SetGroup(string newGroup)
+        {
+            Group = newGroup;
+        }
+
+        internal int CalculateAgeFromBirthYear(int birthYear)
+        {
+            return DateTime.Now.Year - birthYear;
+        }
+    }
+}
